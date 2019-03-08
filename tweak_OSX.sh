203e1e64478e6fcc63dd14b41571b4c00eda03f7
PIDEIL MATTHEW <matthew.pideil.partner@decathlon.com>
2019-03-08 11:46:36 +0100

    improve

--- tweak_OSX.sh
+++ tweak_OSX.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+if [ command -v brew &> /dev/null ]; then
+  echo "brew already installed"
+else
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+fi
+
+echo "updating brew ..."
 brew update
 brew upgrade
+brew upgrade brew-cask
 
+echo "try to install brew stuff"
 brew install midnight-commander wget git git-flow curl ssh-copy-id gnupg tmux coreutils mutt nmap bash-completion
 
 brew cask install iterm2
@@ -12,8 +20,31 @@ brew cask install iterm2
 #brew cask install evernote intellij-idea-ce rescuetime the-unarchiver sequel-pro texturepacker 
 #brew cask install testflight
 
-brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
+brew cleanup && brew cask cleanup
 
-#Voir les fichiers cachés
+echo "set hide file fiewable"
 defaults write com.apple.finder AppleShowAllFiles TRUE
 
+echo "setting shell"
+
+#wget https://raw.githubusercontent.com/Clovis-team/clovis-open-code-extracts/master/utils/Clovis-iTerm2-Color-Scheme.itermcolors
+#exec https://github.com/mbadolato/iTerm2-Color-Schemes/tree/master/schemes
+
+brew install zsh zsh-completions
+#sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" done in chsh
+
+#view https://medium.com/@Clovis_app/configuration-of-a-beautiful-efficient-terminal-and-prompt-on-osx-in-7-minutes-827c29391961
+
+if [ -ne ~/.zsh ];then
+  echo "zsh dont exists install it"
+  brew install zsh zsh-completions
+  cp .
+fi
+
+
+
+
+echo "To change to zsh :"
+echo ""
+echo "chsh -s $(grep /zsh$ /etc/shells | tail -1)"
+echo ""
